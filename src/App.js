4e35c58d9bca655ea58b8f8225c44da5cdf2f449
akumar397800 <akumar397800@gmail.com>
2023-02-16 21:20:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import AboutUs from './components/AboutUs';
+import Articles from './components/Articles';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <>
+      <Hero/>
+      <AboutUs/>
+      <Articles/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
